f9a43e0e6798917e548bfe2e1112bd087e1a72c3
Mochamad Alfin Rakhmat Fadillah <114910587+alfinfadillah@users.noreply.github.com>
2022-10-29 06:33:00 +0700

    Add files via upload

---
+++ alfin.js
@@ -0,0 +1,36 @@
+// Exercise reassignment - Perhitungan
+// Kamu memiliki tugas untuk mengkalkulasikan variabel-variabel di bawah sesuai dengan instruksi console.log yang berlaku (WAJIB MENGGUNAKAN VARIABEL YANG ADA)
+
+var angkaPertama = 2
+var angkaKedua = 4
+var angkaKetiga = 5
+var angkaKeempat = 7
+
+// 1. Tulis kode diline setelah comment ini
+angkaPertama = angkaKetiga + angkaKedua - angkaPertama
+console.log(angkaPertama) // 7
+
+// 2. Tulis kode diline setelah comment ini
+angkaKedua = angkaKedua - angkaKetiga + angkaPertama -angkaKetiga
+console.log(angkaKedua) // 1
+
+// 3. Tulis kode diline setelah comment ini
+angkaKeempat = angkaPertama + angkaKeempat
+console.log(angkaKeempat) // 14
+
+// 4. Tulis kode diline setelah comment ini
+angkaKetiga = angkaKetiga * angkaKeempat
+console.log(angkaKetiga) // 70
+
+// ====================================================
+// Exercise reassignment - Konkantinasi
+// Gabungkanlah kata-kata dan kalimat dalam variabel yang telah disediakan menjadi hasil yang diharapkan 
+
+var hasil = ''
+var kalimatPertama = 'Halo'
+var kalimatKedua = 'namaku'
+var kalimatKetiga = 'Alfin'
+var kalimatKeempat = ', aku sedang belajar coding'
+
+hasil = kalimatPertama + " " + kalimatKedua + " " + kalimatKetiga + kalimatKeempat
+console.log(hasil) // Halo namaku Alfin, aku sedang belajar coding 
